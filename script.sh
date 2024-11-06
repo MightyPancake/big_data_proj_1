61ab9cefe137a38c11dbdbeb42c7ddf57ece2742
MightyPancake <amightypancake@gmail.com>
2024-11-06 23:49:32 +0100

    adjusted script

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
-wget http://www.cs.put.poznan.pl/kjankiewicz/bigdata/projekty/zestaw7.zip
-unzip zestaw7
-rm zestaw7.zip
+wget http://www.cs.put.poznan.pl/kjankiewicz/bigdata/projekty/zestaw3.zip
+unzip zestaw3
+rm zestaw3.zip
 hadoop fs -mkdir -p input
 hadoop fs -put ./input/* input
 
-wget https://raw.githubusercontent.com/Free-Rat/big-data-project-1/refs/heads/main/mapper.py
-wget https://raw.githubusercontent.com/Free-Rat/big-data-project-1/refs/heads/main/reducer.py
+wget https://raw.githubusercontent.com/MightyPancake/big_data_proj_1/refs/heads/main/mapper.py
+wget https://raw.githubusercontent.com/MightyPancake/big_data_proj_1/refs/heads/main/reducer.py
 sudo chmod +x mapper.py
 sudo chmod +x reducer.py
 
