318ca3ce6b2341d2036ffc40bf77b3d9374a8a43
MightyPancake <amightypancake@gmail.com>
2024-11-08 23:08:42 +0100

    changed back to full python3

--- map-reduce.sh
+++ map-reduce.sh
@@ -8,9 +8,9 @@ mapred streaming \
     -files mapper.py,reducer.py,combiner.py \
     -input input/datasource1 \
     -output output \
-    -mapper mapper.py \
-    -combiner combiner.py \
-    -reducer python3 reducer.py \
+    -mapper "python3 mapper.py" \
+    -combiner "python3 combiner.py" \
+    -reducer "python3 reducer.py" \
     -jobconf mapreduce.job.output.key.comparator.class=org.apache.hadoop.mapreduce.lib.partition.KeyFieldBasedComparator \
     -jobconf stream.num.map.output.key.fields=2 \
     -jobconf mapreduce.partition.keycomparator.options="-k1,1 -k2,2"
