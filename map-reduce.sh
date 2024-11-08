bff422e0ae59b7ce1e98212c1e946a1398cfc689
MightyPancake <amightypancake@gmail.com>
2024-11-08 22:59:22 +0100

    fixed stuff

--- map-reduce.sh
+++ map-reduce.sh
@@ -4,13 +4,13 @@
 hadoop fs -rm -r output
 
 # Uruchom zadanie map-reduce z hadoop-streaming
-hadoop streaming \
+mapred streaming \
     -files mapper.py,reducer.py,combiner.py \
     -input input/datasource1 \
     -output output \
-    -mapper "python3 mapper.py" \
-    -combiner "python3 combiner.py" \
-    -reducer "python3 reducer.py" \
+    -mapper mapper.py \
+    -combiner combiner.py \
+    -reducer python3 reducer.py \
     -jobconf mapreduce.job.output.key.comparator.class=org.apache.hadoop.mapreduce.lib.partition.KeyFieldBasedComparator \
     -jobconf stream.num.map.output.key.fields=2 \
     -jobconf mapreduce.partition.keycomparator.options="-k1,1 -k2,2"
