190f609e36dcb3f1f0aa07e1fd21876c39294f3b
MightyPancake <amightypancake@gmail.com>
2024-11-08 22:49:03 +0100

    updated things 2

--- map-reduce.sh
+++ map-reduce.sh
@@ -1,17 +1,16 @@
+#!/bin/bash
+
+# Usuń istniejący folder output (jeśli istnieje)
 hadoop fs -rm -r output
 
-mapred streaming \
+# Uruchom zadanie map-reduce z hadoop-streaming
+hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming.jar \
     -files mapper.py,reducer.py,combiner.py \
     -input input/datasource1 \
     -output output \
     -mapper "python3 mapper.py" \
     -combiner "python3 combiner.py" \
     -reducer "python3 reducer.py" \
-    -D mapreduce.job.output.key.comparator.class=org.apache.hadoop.mapreduce.lib.partition.KeyFieldBasedComparator \
-    -D stream.num.map.output.key.fields=2 \
-    -D mapreduce.partition.keycomparator.options="-k1,1 -k2,2"
-
-# Last 3 switches make it so I don't have to merge 2 keys into one (which would be extremely dumb, but is likely what people do lol)
-
-# Ignore this, it was just a local test
-# cat input/datasource1/* | python mapper.py | sort -k1,1 | python reducer.py > output
+    -jobconf mapreduce.job.output.key.comparator.class=org.apache.hadoop.mapreduce.lib.partition.KeyFieldBasedComparator \
+    -jobconf stream.num.map.output.key.fields=2 \
+    -jobconf mapreduce.partition.keycomparator.options="-k1,1 -k2,2"
